04a657927ceff718790d4dba2dc2976531e679cd
wellitobraian <wellitonbraian@gmail.com>
2023-09-27 16:42:19 -0300

    :Welliton: initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+   const html = document.documentElement
+    html.classList.toggle("light")
+ 
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    //substituir a imagem
+    if (html.classList.contains("light"))  {
+
+    //Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    } 
+    else {
+
+    //Se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+    }
+} 
